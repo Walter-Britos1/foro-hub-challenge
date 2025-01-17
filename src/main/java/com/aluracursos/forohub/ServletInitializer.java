457c7e7c93cc2b84811f2316fede9a8dff7a07ac
Walter-Britos1 <walterbritos418@gmail.com>
2025-01-17 03:16:22 -0300

    initial commit

---
+++ src/main/java/com/aluracursos/forohub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.aluracursos.forohub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ForohubApplication.class);
+	}
+
+}
